d02b382f7e20bc2f4016ef64ee5771851572d2dc
dtg01100 <dlafreniere349@gmail.com>
2025-11-23 15:49:44 -0500

    Update to v1.0.1 with lib path fix

--- Formula/dcutil.rb
+++ Formula/dcutil.rb
@@ -1,8 +1,8 @@
 class Dcutil < Formula
   desc "Complete Devcontainer Specification Implementation"
   homepage "https://github.com/dtg01100/dcutil"
-  url "https://github.com/dtg01100/dcutil/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "53db7e1544361c17936c23f6bb55c74a891708993a9e560b069b2a358b3aff12"
+  url "https://github.com/dtg01100/dcutil/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "3b70907440c36f97afafc7d840c62144705f5e86bf0522786655d561a6d23dd6"
   license "MIT"
 
   def install
