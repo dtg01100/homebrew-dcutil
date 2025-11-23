071dcc6aab63aa19fbfe49be3129154d7501b659
dtg01100 <dlafreniere349@gmail.com>
2025-11-23 15:50:35 -0500

    Update to v1.0.2

--- Formula/dcutil.rb
+++ Formula/dcutil.rb
@@ -1,8 +1,8 @@
 class Dcutil < Formula
   desc "Complete Devcontainer Specification Implementation"
   homepage "https://github.com/dtg01100/dcutil"
-  url "https://github.com/dtg01100/dcutil/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "3b70907440c36f97afafc7d840c62144705f5e86bf0522786655d561a6d23dd6"
+  url "https://github.com/dtg01100/dcutil/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "5fdba221e3a0b9ee7433fc25a346810cfc3302a5624c8684adfcfca2625628f4"
   license "MIT"
 
   def install
