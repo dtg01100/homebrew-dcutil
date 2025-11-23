33f2a37eac7cdcbaaec62895308dd3a6c7893fb5
dtg01100 <dlafreniere349@gmail.com>
2025-11-23 15:46:54 -0500

    Add dcutil formula

---
+++ Formula/dcutil.rb
@@ -0,0 +1,15 @@
+class Dcutil < Formula
+  desc "Complete Devcontainer Specification Implementation"
+  homepage "https://github.com/dtg01100/dcutil"
+  url "https://github.com/dtg01100/dcutil/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "53db7e1544361c17936c23f6bb55c74a891708993a9e560b069b2a358b3aff12"
+  license "MIT"
+
+  def install
+    bin.install "dcutil"
+  end
+
+  test do
+    system "#{bin}/dcutil", "--help"
+  end
+end
