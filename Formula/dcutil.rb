42cbe2d43a24d7ebcda61062c7bc5324aabacaea
dtg01100 <dlafreniere349@gmail.com>
2025-11-24 00:54:06 -0500

    fix: update SHA256 checksum for v1.1.1

--- Formula/dcutil.rb
+++ Formula/dcutil.rb
@@ -2,7 +2,7 @@ class Dcutil < Formula
   desc "Enhanced devcontainer utility with advanced features"
   homepage "https://github.com/dtg01100/dcutil"
   url "https://github.com/dtg01100/dcutil/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "CHECKSUM_WILL_BE_AUTO_UPDATED_ON_RELEASE"
+  sha256 "dae4707a003852182c0b3718ae5e76f3819d6b159bba392f1b8398dc6924b39b"
   license "MIT"
 
   # Dependencies (alphabetical order)
